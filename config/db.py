55ca50a51b4d442e79b1cd20836062bf60b7e8bf
Rajat <rajatawadhut@gmail.com>
2021-06-20 14:00:09 +0530

    New Updates

--- config/db.py
+++ config/db.py
@@ -2,4 +2,5 @@ from sqlalchemy import create_engine, MetaData
 engine = create_engine("mysql+pymysql://root@localhost:3306/lopa")
 meta = MetaData()
 meta.create_all(bind=engine)
+
 conn = engine.connect()
